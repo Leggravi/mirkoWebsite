a3b68d60adba63047a5f5254576d6c3afe89981a
Leggravi <hansheinemann2.0@gmail.com>
2025-07-28 16:23:49 +0200

    app 2.0

---
+++ _posts/2025-09-10-titel (copy).md
@@ -0,0 +1,16 @@
+---
+layout: post
+title: "Kartoffeln..."
+date: 2025-01-02
+---
+ ich essse gerne
+ # k
+ # a
+ # r
+ # t
+ # o
+ # f
+ # f
+ # e
+ l #
+ n!!!!
--- _site/service-worker.js
+++ _site/service-worker.js
@@ -1,48 +1,41 @@
 const CACHE_NAME = 'mirko-cache-v1';
-const OFFLINE_URL = '/offline.html';
+const OFFLINE_URL = '/offline.html'; // offline fallback
+const ENABLE_SW = true; // Schalter für SW aktiv/inaktiv
 
 const urlsToCache = [
   '/',
-  '/assets/css/style.css',
-  '/assets/img/mirko.jpg',
-  OFFLINE_URL,
-  // weitere wichtige Dateien hier ergänzen!
+  '/index.html',
+  '/main.css',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png',
+  OFFLINE_URL
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .catch(() => caches.match(OFFLINE_URL))
+if (ENABLE_SW) {
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
-  }
-});
+  });
 
-// Cache-Reset über Nachricht
-self.addEventListener('message', event => {
-  if (event.data === 'clear-cache') {
-    caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
-  }
-});
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(event.request).then(response => {
+          return response || caches.match(OFFLINE_URL);
+        });
+      })
+    );
+  });
+
+  self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(keys =>
+        Promise.all(
+          keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        )
+      )
+    );
+  });
+}
+
