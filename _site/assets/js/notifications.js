44b0942742b93226a40f240b5b4e591c9dbd43b3
Leggravi <hansheinemann2.0@gmail.com>
2025-08-28 15:30:30 +0200

    anaylsen
    google und so

--- _site/assets/js/notifications.js
+++ _site/assets/js/notifications.js
@@ -1,33 +1,10 @@
 window.NOTIFICATIONS = [
   {
-    id: "2025-07-28-neuer-blogpost",
-    message: "Neuer Blogpost: Brasilien-Abenteuer!",
-    url: "/blog/2025-07-28-neuer-blogpost/",
-    type: "info"
-  },
-  {
     id: "2025-07-20-willkommen",
     message: "Willkommen auf meiner Website!",
     url: "/",
     type: "success"
-  },
-  {
-    id: "2025-08-01-super-news",
-    message: "Super News: Jetzt mit Cookie-Benachrichtigungen!",
-    url: "/blog/2025-08-01-super-news/",
-    type: "warning"
-  },
-  {
-    id: "2025-08-05-neue-funktion",
-    message: "Neue Funktion: Benachrichtigungen für neue Blogposts!",
-    url: "/blog/2025-08-05-neue-funktion/",
-    type: "info"
-  },
-  {
-    id: "2025-08-10-wartungsarbeiten",
-    message: "Wartungsarbeiten am 15. August von 2 bis 4 Uhr.",
-    url: "/blog/2025-08-10-wartungsarbeiten/",
-    type: "warning"
   }
   // Weitere Benachrichtigungen einfach ergänzen
+  //types : info, success, warning, error
 ];
